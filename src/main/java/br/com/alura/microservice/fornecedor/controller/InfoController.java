502bf1396c9e34cdcd0a441ee0488d9bb0089a96
Daniel-Nasciment <daniel.fecaf@outlook.com>
2022-07-22 17:15:08 -0300

    Iniciando com logs

--- src/main/java/br/com/alura/microservice/fornecedor/controller/InfoController.java
+++ src/main/java/br/com/alura/microservice/fornecedor/controller/InfoController.java
@@ -1,5 +1,7 @@
 package br.com.alura.microservice.fornecedor.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,12 +16,15 @@ import br.com.alura.microservice.fornecedor.service.InfoService;
 @RequestMapping(value = "/info")
 public class InfoController {
 	
+	private static final Logger LOG = LoggerFactory.getLogger(InfoController.class);
+	
 	@Autowired
 	private InfoService infoService;
 	
 	@GetMapping(value = "/{estado}")
 	public ResponseEntity<?> devolveEndereco(@PathVariable String estado) {
 		
+		LOG.info("recebido o pedido de informações do fornecedor de {}", estado);
 		
 		Fornecedor fornecedor = infoService.getInfoPorEstado(estado);
 		
