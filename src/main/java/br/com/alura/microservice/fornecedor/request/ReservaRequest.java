75908f0773a7ac28dc2f716a4b758e67a0f3f3d6
Daniel-Nasciment <daniel.fecaf@outlook.com>
2022-07-22 15:27:52 -0300

    Importando pedido e produtos

---
+++ src/main/java/br/com/alura/microservice/fornecedor/request/ReservaRequest.java
@@ -0,0 +1,25 @@
+package br.com.alura.microservice.fornecedor.request;
+
+public class ReservaRequest {
+
+	public Integer idReserva;
+	
+	public Integer tempoDePreparo;
+
+	public Integer getIdReserva() {
+		return idReserva;
+	}
+
+	public void setIdReserva(Integer idReserva) {
+		this.idReserva = idReserva;
+	}
+
+	public Integer getTempoDePreparo() {
+		return tempoDePreparo;
+	}
+
+	public void setTempoDePreparo(Integer tempoDePreparo) {
+		this.tempoDePreparo = tempoDePreparo;
+	}
+	
+}
