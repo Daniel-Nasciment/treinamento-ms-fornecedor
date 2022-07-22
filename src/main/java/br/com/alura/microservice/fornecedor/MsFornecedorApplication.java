7cb91a84eb982bf4bab836a038a55f828bc4e9e7
Daniel-Nasciment <daniel.fecaf@outlook.com>
2022-07-21 22:31:36 -0300

    Implementando o eureka client

--- src/main/java/br/com/alura/microservice/fornecedor/MsFornecedorApplication.java
+++ src/main/java/br/com/alura/microservice/fornecedor/MsFornecedorApplication.java
@@ -2,8 +2,10 @@ package br.com.alura.microservice.fornecedor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MsFornecedorApplication {
 
 	public static void main(String[] args) {
