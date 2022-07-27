3a6dc178c7baaa8bc76d965d424d67de1558572c
Daniel-Nasciment <daniel.fecaf@outlook.com>
2022-07-27 20:11:01 -0300

    Implementando a validacao do token e configurando urls que necessitam de token

--- src/main/java/br/com/alura/microservice/fornecedor/MsFornecedorApplication.java
+++ src/main/java/br/com/alura/microservice/fornecedor/MsFornecedorApplication.java
@@ -3,9 +3,11 @@ package br.com.alura.microservice.fornecedor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableResourceServer
 public class MsFornecedorApplication {
 
 	public static void main(String[] args) {
